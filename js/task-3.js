3716a52f223f0286fed4ee1f73a1e41e484968de
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2023-11-15 00:35:01 +0200

    theard task

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const filterArray = function (numbers, value) {
+  let maxValues = [];
+  for (const item of numbers) {
+    if (item > value) {
+      maxValues.push(item);
+    }
+  }
+  return maxValues;
+};
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
